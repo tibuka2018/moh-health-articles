437ae2cdbe6ee68b209657d2dc696e0119855540
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-17 18:25:40 +0300

    Create section model and migration

---
+++ database/migrations/2016_10_17_151935_create_sections_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sections', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('user_id')->unsigned();
+            $table->integer('article_id')->unsigned();
+
+            $table->string('title');
+            $table->string('slug');
+            $table->text('content');
+
+            $table->integer('image_id')->unsigned()->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sections');
+    }
+}
