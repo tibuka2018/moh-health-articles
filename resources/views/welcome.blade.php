c0f175bbb5b62c0268f008c8c7a6c4303189657d
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-17 14:02:09 +0300

    Create card to display article

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,114 @@
 			<div class="row">
 				{{-- Articles --}}
 				<div class="col-xs-12 col-sm-9">
-					Articles
+					
+				<div class="row">
+					<div class="col-xs-12 col-sm-4">
+						<div class="card hovercard">
+						   <img src="http://lorempixel.com/300/200/nature/1" alt=""/>
+						   <div class="avatar">
+						      <img src="http://lorempixel.com/80/80/nature/1" alt="" />
+						   </div>
+						   <div class="info">
+						      <div class="title">
+						         The Title
+						      </div>
+						      <div class="desc">Lorem ipsum</div>
+						   </div>
+						   <div class="bottom">
+						      <button class="btn btn-default">Button</button>
+						   </div>
+						</div>						
+					</div>
+					<div class="col-xs-12 col-sm-4">
+						<div class="card hovercard">
+						   <img src="http://lorempixel.com/300/200/nature/1" alt=""/>
+						   <div class="avatar">
+						      <img src="http://lorempixel.com/80/80/nature/1" alt="" />
+						   </div>
+						   <div class="info">
+						      <div class="title">
+						         The Title
+						      </div>
+						      <div class="desc">Lorem ipsum</div>
+						   </div>
+						   <div class="bottom">
+						      <button class="btn btn-default">Button</button>
+						   </div>
+						</div>						
+					</div>
+					<div class="col-xs-12 col-sm-4">
+						<div class="card hovercard">
+						   <img src="http://lorempixel.com/300/200/nature/1" alt=""/>
+						   <div class="avatar">
+						      <img src="http://lorempixel.com/80/80/nature/1" alt="" />
+						   </div>
+						   <div class="info">
+						      <div class="title">
+						         The Title
+						      </div>
+						      <div class="desc">Lorem ipsum</div>
+						   </div>
+						   <div class="bottom">
+						      <button class="btn btn-default">Button</button>
+						   </div>
+						</div>						
+					</div>										
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-sm-4">
+						<div class="card hovercard">
+						   <img src="http://lorempixel.com/300/200/nature/1" alt=""/>
+						   <div class="avatar">
+						      <img src="http://lorempixel.com/80/80/nature/1" alt="" />
+						   </div>
+						   <div class="info">
+						      <div class="title">
+						         The Title
+						      </div>
+						      <div class="desc">Lorem ipsum</div>
+						   </div>
+						   <div class="bottom">
+						      <button class="btn btn-default">Button</button>
+						   </div>
+						</div>						
+					</div>
+					<div class="col-xs-12 col-sm-4">
+						<div class="card hovercard">
+						   <img src="http://lorempixel.com/300/200/nature/1" alt=""/>
+						   <div class="avatar">
+						      <img src="http://lorempixel.com/80/80/nature/1" alt="" />
+						   </div>
+						   <div class="info">
+						      <div class="title">
+						         The Title
+						      </div>
+						      <div class="desc">Lorem ipsum</div>
+						   </div>
+						   <div class="bottom">
+						      <button class="btn btn-default">Button</button>
+						   </div>
+						</div>						
+					</div>
+					<div class="col-xs-12 col-sm-4">
+						<div class="card hovercard">
+						   <img src="http://lorempixel.com/300/200/nature/1" alt=""/>
+						   <div class="avatar">
+						      <img src="http://lorempixel.com/80/80/nature/1" alt="" />
+						   </div>
+						   <div class="info">
+						      <div class="title">
+						         The Title
+						      </div>
+						      <div class="desc">Lorem ipsum</div>
+						   </div>
+						   <div class="bottom">
+						      <button class="btn btn-default">Button</button>
+						   </div>
+						</div>						
+					</div>										
+				</div>				
+
 				</div>
 				{{-- Sidebar --}}
 				<div class="col-xs-12 col-sm-3">
