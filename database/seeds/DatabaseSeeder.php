2f8f3a9271a14b62da2caa18c6b63a0fc2fd57a9
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-17 22:26:33 +0300

    Another way of seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Database\Seeder;
 
+use App\User;
+use App\Article;
+use App\Category;
+use App\Image;
+use App\Section;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,10 +17,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(ArticlesTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
-        $this->call(ImagesTableSeeder::class);
-        $this->call(SectionsTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
+        User::truncate();
+        factory(App\User::class, 10)->create();        
+        // $this->call(ArticlesTableSeeder::class);
+        Article::truncate();
+        factory(App\Article::class, 17)->create();        
+        // $this->call(CategoriesTableSeeder::class);
+        Category::truncate();
+        factory(App\Category::class, 10)->create();        
+        // $this->call(ImagesTableSeeder::class);
+        Image::truncate();
+        factory(App\Image::class, 20)->create();        
+        // $this->call(SectionsTableSeeder::class);
+        Section::truncate();
+        factory(App\Section::class, 100)->create();        
     }
 }
