9e6bbec2b86260a810868a464a59167c12db4e43
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-17 14:28:59 +0300

    Create footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,47 @@
-footer
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12 col-sm-3">
+			<h3>ABOUT US</h3>
+			<p>
+				Dead cardboard girl advert dolphin singularity sentient bomb math-receding. Geodesic gang city into computer knife boy concrete paranoid. Free-market physical industrial grade carbon chrome face forwards tiger-team.  
+			</p>
+		</div>
+		<div class="col-xs-12 col-sm-3">
+			<h3>QUICK LINKS</h3>
+			<ul class="list-unstyled">
+				<li><a href="#">Health Facilities</a></li>
+				<li><a href="#">ICT Support</a></li>
+				<li><a href="#">Health Services</a></li>
+				<li><a href="#">Health Indicators</a></li>
+				<li><a href="#">Disease Report</a></li>
+			</ul>
+		</div>
+		<div class="col-xs-12 col-sm-3">
+			<h3>INFORMATION</h3>
+			<ul class="list-unstyled">
+				<li><a href="#">News</a></li>
+				<li><a href="#">Events Gallery</a></li>
+				<li><a href="#">Projects</a></li>
+				<li><a href="#">Announcements</a></li>
+				<li><a href="#">Press Releases</a></li>
+			</ul>
+		</div>
+		<div class="col-xs-12 col-sm-3">
+			<h3>NEWSLETTER</h3>
+			<p>
+				Subscribe to our weekly newsletter.
+			</p>
+			<form role="form">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Email Address" name="email">
+				</div>
+				<div class="form-group">
+					<button type="submit" class="btn btn-primary btn-block">Subscribe</button>
+				</div>
+			</form>
+		</div>
+	</div>
+	<div class="row">
+		
+	</div>
+</div>
