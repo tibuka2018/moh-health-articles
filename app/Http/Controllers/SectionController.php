1eb3bc5b14690f1a7194c79168853b5ed6a63a86
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-18 10:17:29 +0300

    Create a new section if new section button is clicked

--- app/Http/Controllers/SectionController.php
+++ app/Http/Controllers/SectionController.php
@@ -64,7 +64,41 @@ class SectionController extends Controller
 	            return redirect('home');
     		}
     	} else {
-    		return 'new section';
+			// check if an image is submitted
+    		if ($request->hasFile('image')) {
+    			// upload that image
+
+	            $path = $request->image->store('images');
+
+	            $image = new Image();
+	            $image->user_id = Auth::user()->id;
+	            $image->url = $path;
+	            $image->save();
+
+	            // insert that section
+	            $section = new Section();
+	            $section->user_id = Auth::user()->id;
+	            $section->article_id = $request->input('article_id');
+	            $section->title = $request->input('title');
+	            $section->slug  =  str_slug($request->input('title') . ' ' . Auth::user()->id, '-');
+	            $section->content = $request->input('content');
+	            $section->image_id = $image->id;
+	            $section->save();
+
+	            return redirect('articles/' . $request->input('article_id') . '/sections/new');
+
+    		} else {
+	            // insert that section
+	            $section = new Section();
+	            $section->user_id = Auth::user()->id;
+	            $section->article_id = $request->input('article_id');
+	            $section->title = $request->input('title');
+	            $section->slug  =  str_slug($request->input('title') . ' ' . Auth::user()->id, '-');
+	            $section->content = $request->input('content');
+	            $section->save();
+
+	            return redirect('articles/' . $request->input('article_id') . '/sections/new');
+    		}
     	}
     }
 }
