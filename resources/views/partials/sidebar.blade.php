eebea90b8e342bf90a3009cb772fdd825a5888be
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-18 20:45:25 +0300

    Improve sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="media">
+{{-- <div class="media">
 	<a class="pull-left" href="#">
 		<img class="media-object" src="http://placehold.it/50x50" alt="Image">
 	</a>
@@ -9,3 +9,10 @@
 		<p><a href="{{ url('articles/' . $latest_articles->slug) }}">Read &raquo;</a></p>
 	</div>
 </div>	
+ --}}
+ <div class="list-group">
+ 	<a href="{{ url('articles/' . $latest_articles->slug) }}" class="list-group-item">
+ 		<h5 class="list-group-item-heading">{{ $latest_articles->title }}</h5>
+ 		<p class="list-group-item-text">{{ $latest_articles->created_at->diffForHumans() }} <strong>by</strong> {{ $latest_articles->user->name }}</p>
+ 	</a>
+ </div>
