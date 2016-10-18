e80a276d3213e6b3c2a0ace79f11656f7b1159f4
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-19 00:38:47 +0300

    Protect routes

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -12,6 +12,11 @@ use Illuminate\Support\Facades\Auth;
 
 class VideoController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
